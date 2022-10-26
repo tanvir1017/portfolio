470809a8cf5a7d4472eb9664be6192bbce6ddd96
tanvir1017 <tanvir.hossaindev1@gmail.com>
2022-10-27 00:58:45 +0600

    test: 💍 Much awaited portfolio initiated 😎😎
    Much awaited portfolio initiated 😎😎
    
    BREAKING CHANGE: 🧨 0
    
    ✅ Closes: 0

---
+++ app/root.jsx
@@ -0,0 +1,35 @@
+import styles from "./styles/app.css";
+const {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} = require("@remix-run/react");
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+export const meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <h1 className="text-green-500 font-bold text-5xl text-center mt-12">
+      This is going to be the portfolio of Tanvir Hossain 😇
+    </h1>
+  );
+}
